b6c9906a0abbf7ba11ec401abef8056cbe901653
Emily Frost <emfrost@gmail.com>
2019-05-25 19:22:54 -0600

    every other gif is functional

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,50 +1,64 @@
 var topics = ["dogs", "cats", "bears"]
 var APIkey = "fTPjGEwjqFzTHWzs9U8biFX3uSz0ZCpq"
 
-
-//render buttons to the page
-console.log("giphy.js lnked!")
-function renderButtons (){
+//render buttons to the page for each topic in array
+function renderButtons() {
     $(".buttons").empty();
     for (var i = 0; i < topics.length; i++) {
         var topic = topics[i];
-        var newButton = $("<button>"); 
-            newButton.text(topic);
-            newButton.addClass("topic-button");
-            newButton.attr("data-title", topic);
+        var newButton = $("<button>");
+        newButton.text(topic);
+        newButton.addClass("topic-button");
+        newButton.attr("data-title", topic);
         $(".buttons").append(newButton);
     }
 };
 
+//puts user searchterm in a button on the page & adds it to the topics array
 $(".Userinput").on("submit", function (event) {
-    event.preventDefault ();
+    event.preventDefault();
     var topic = $("#Usertext").val().trim();
     console.log(topic);
     topics.push(topic);
     renderButtons();
-    $("#Usertext").val(""); 
+    $("#Usertext").val("");
 });
 
-$(document).on("click", ".topic-button", function (){
+//runs the ajax method when a button is clicked
+$(document).on("click", ".topic-button", function () {
     var searchTerm = $(this).text();
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + APIkey + "&limit=10"
     console.log("button was clicked");
     console.log($(this).text())
-    
+
     $.ajax({
-        method: "GET", 
+        method: "GET",
         url: queryURL
-    }).then(function(response) {
-      console.log(response.data);
-      for (var i = 0; i < response.data.length; i++) {
-        console.log(response.data[i].images.fixed_width.url)
-        $(".gifs").append("<img src = '" + response.data[i].images.fixed_width.url + "'>")
-      }
+    }).then(function (response) {
+        console.log(response.data);
+        //loops through the gifs and ascribes values to them then puts them on the page
+        for (var i = 0; i < response.data.length; i++) {
+            var stillImage = response.data[i].images.fixed_width_still.url;
+            var animatedImage = response.data[i].images.fixed_width.url;
+            var mainURL = stillImage;
+            var gifImage = $("<img>").attr("src", mainURL).attr("data-still", stillImage).attr("data-animate", animatedImage).attr("data-state", "still").addClass("gif");
+            var rating = response.data[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            $(".gifs").prepend(p, gifImage);
+            //stops and starts gifs with on.click event
+            $(".gif").on("click", function () {
+                var state = $(this).attr("data-state");
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            });
+        }
     });
 });
 
 
 renderButtons();
-
-//give images a class and a data-attribute that will store the still image; replace the source with the data atttribute 
-//create click events. 
